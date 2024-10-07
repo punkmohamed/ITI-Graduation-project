e00ad46d202f41590b40f5c30ae5c4a36a4e20b4
punkmohamed <punkmohamed22@gmail.com>
2024-10-07 17:36:14 +0200

    commit

--- client/user/src/components/category/Category.jsx
+++ client/user/src/components/category/Category.jsx
@@ -9,7 +9,7 @@ export default function Category({ category }) {
   const { language } = useSelector(state => state.lang)
   const { mode } = useSelector(state => state.mode)
   return (
-    <div className="col-span-12 md:col-span-6 lg:col-span-4 mt-3 mx-3">
+    <div className="col-span-12 md:col-span-6 lg:col-span-4 mt-3 mx-1 md:mx-1">
       <Link to={`/menu/${category._id}`}>
         <Card className={`max-w-sm m-auto ${mode == 'light' ? 'catBackLight' : 'catBack'} bg-no-repeat bg-center bg-cover p-24 `}>
           <h5 className="text-2xl text-center font-bold tracking-tight ">
