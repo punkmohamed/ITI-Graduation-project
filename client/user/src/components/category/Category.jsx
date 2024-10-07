11054a3a028117443e620f3eec222fc64da05fca
punkmohamed <punkmohamed22@gmail.com>
2024-10-07 17:47:07 +0200

    commit

--- client/user/src/components/category/Category.jsx
+++ client/user/src/components/category/Category.jsx
@@ -10,8 +10,8 @@ export default function Category({ category }) {
   const { mode } = useSelector(state => state.mode)
   return (
     <div className="col-span-12 md:col-span-6 lg:col-span-4 mt-3 mx-1 md:mx-1">
-      <Link to={`/menu/${category._id}`}>
-        <Card className={`max-w-sm m-auto ${mode == 'light' ? 'catBackLight' : 'catBack'} bg-no-repeat bg-center bg-cover p-24 `}>
+      <Link to={`/menu/${category._id}`} className="bg-transparent">
+        <Card className={`max-w-sm m-auto bg-transparent ${mode == 'light' ? 'catBackLight' : 'catBack h-[337px]'} bg-no-repeat bg-center bg-contain md:bg-cover  p-24 `}>
           <h5 className="text-2xl text-center font-bold tracking-tight ">
             {language == "en" ? category?.name?.en : category?.name?.ar}
           </h5>
